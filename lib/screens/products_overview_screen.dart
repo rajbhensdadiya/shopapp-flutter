8d8ca9c48d81b4bd1859c068dfe44b50cc5293a1
rajpate1225 <rkumar79441@gmail.com>
2021-03-09 12:36:06 +0530

    Initial commit

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+import '../widgets/products_grid.dart';
+// import '../providers/products.dart';
+
+enum filterOptions {
+  Favorites,
+  All,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+  @override
+  Widget build(BuildContext context) {
+    // final productsContainer = Provider.of<Products>(context, listen: false);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Shop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (filterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == filterOptions.Favorites) {
+                  // productsContainer.showFavoritesOnly();
+                  _showOnlyFavorites = true;
+                } else {
+                  // productsContainer.showAll();
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: filterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: filterOptions.All,
+              ),
+            ],
+          ),
+        ],
+      ),
+      body: ProductsGrid(_showOnlyFavorites),
+    );
+  }
+}
